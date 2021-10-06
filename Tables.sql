5452edd0c12342d5db7229255d6c9a5eb3952f50
IDemkiv <irina.nikolyaevna@gmail.com>
2021-10-06 21:56:54 +0300

    Added foreign keys and fixed mismatching columns

--- Tables.sql
+++ Tables.sql
@@ -1,6 +1,6 @@
 /*Database objects creation*/
 
-drop table  if exists [Demkiv].[EMPLOYEES]
+drop table  if exists [Demkiv].[PERSONS]
 
 create table [Demkiv].[PERSONS] (
   [PersonId] [bigint] IDENTITY(1,1) primary key NOT NULL,
@@ -15,7 +15,7 @@ create table [Demkiv].[PERSONS] (
 drop table  if exists [Demkiv].[PHONES]
 
 create table [Demkiv].[PHONES] (
-  [NumberId] [bigint] IDENTITY(1,1) NOT NULL,
+  [NumberId] [bigint] IDENTITY(1,1) primary key NOT NULL,
   [PhoneNumber] [nchar](30) NOT NULL,
   [NumberType] [nchar](30) NOT NULL,
   [PersonId] [bigint] NOT NULL
@@ -24,7 +24,7 @@ create table [Demkiv].[PHONES] (
 drop table  if exists [Demkiv].[EMAILS]
 
 create table [Demkiv].[EMAILS] (
-  [NumberId] [bigint] IDENTITY(1,1) NOT NULL,
+  [EmailId] [bigint] IDENTITY(1,1) primary key NOT NULL,
   [Email] [nchar](30) NOT NULL,
   [EmailType] [nchar](30) NOT NULL,
   [PersonId] [bigint] NOT NULL
@@ -33,22 +33,23 @@ create table [Demkiv].[EMAILS] (
 drop table  if exists [Demkiv].[EMPLOYEES]
 
 create table [Demkiv].[EMPLOYEES] (
-  [EmployeeId] [bigint] IDENTITY(1,1) NOT NULL,
+  [EmployeeId] [bigint] IDENTITY(1,1) primary key NOT NULL,
   [JobTitle] [nchar](30) NOT NULL,
   [HireDate] [date] NOT NULL,
   [StationId] [int] NOT NULL,
-  [LeadId] [bigint] NOT NULL,
+  [LeadId] [bigint] NULL,
   [SaleTarget] [money] NOT NULL,
   [Status] [tinyint] NOT NULL,
-  [StatusChangeDate] [date] NOT NULL,
+  [StatusChangeDate] [datetime] NOT NULL,
   [TaxNumber] [nchar] (50) NULL,
   [PersonId] [bigint] NOT NULL
  )
 
+
 drop table  if exists [Demkiv].[CUSTOMERS]
 
 create table [Demkiv].[CUSTOMERS] (
-  [CustomerId] [bigint] IDENTITY(1,1) NOT NULL,
+  [CustomerId] [bigint] IDENTITY(1,1) primary key  NOT NULL,
   [ServiceType] [nchar](30) NOT NULL,
   [ServiceStartDate] [date] NOT NULL,
   [EmployeeId] [bigint] NOT NULL,
@@ -60,7 +61,7 @@ create table [Demkiv].[CUSTOMERS] (
 drop table  if exists [Demkiv].[CARDS]
 
 create table [Demkiv].[CARDS] (
-  [CardNumber] [bigint]  NOT NULL, 
+  [CardNumber] [bigint] primary key NOT NULL, 
   [CustomerId] [bigint]  NOT NULL,
   [Status] [tinyint] NOT NULL,
   [StatusChangeDate] [date] NOT NULL,
@@ -73,50 +74,97 @@ drop table  if exists [Demkiv].[PRODUCTS]
 create table [Demkiv].[PRODUCTS] (
   [VendorID] [bigint]  NOT NULL, 
   [ProductId] [bigint]  NOT NULL,
-  [ProductName] [nchar](50) NOT NULL,
+  [ProductName] [nchar] (50) NOT NULL,
   [ProductPrice] [money] NOT NULL,
   [ProductAmount] [bigint] NULL,
  )
 
-drop table  if exists [Demkiv].[Orders]
+ alter table [Demkiv].[PRODUCTS] 
+ add constraint pk_products primary key ([VendorID],[ProductId])
+
+
+drop table  if exists [Demkiv].[ORDERS]
 
 create table [Demkiv].[ORDERS] (
-  [OrderID] [bigint]  NOT NULL, 
+  [OrderID] [bigint] primary key NOT NULL, 
   [CardId] [bigint]  NOT NULL,
-  [StationId] [bigint]  NOT NULL,
+  [StationId] [int]  NOT NULL,
   [PersonId] [bigint]  NOT NULL,
   [OrderDate] [date] NOT NULL
  )
 
-drop table  if exists [Demkiv].[OrderDetails]
+drop table  if exists [Demkiv].[ORDERDETAILS]
 
 create table [Demkiv].[ORDERDETAILS] (
   [OrderID] [bigint]  NOT NULL, 
-  [OrderDetailId] [bigint]  NOT NULL,
+  [OrderDetailId] [bigint] primary key NOT NULL,
   [VendorId] [bigint]  NOT NULL,
   [ProductId] [bigint]  NOT NULL,
   [OrderAmount] [int] NOT NULL,
   [TotalSum] [int] NOT NULL
  )
 
-drop table  if exists [Demkiv].[Stations]
+drop table  if exists [Demkiv].[STATIONS]
 
 create table [Demkiv].[STATIONS] (
-  [StationId] [bigint] IDENTITY(1,1) NOT NULL,
+  [StationId] [int] IDENTITY(1,1) primary key NOT NULL,
   [District] [nchar](30) NOT NULL,
   [Address] [nchar](50) NOT NULL,
   [LeadId] [bigint] NOT NULL
  )
 
-/*alter table [EMPLOYEES]
-add constraint  pk_id primary key (PersonId)
+ --Creation all the foreign keys
+ GO
+
+alter table [Demkiv].[PHONES]
+add constraint  fk_PHONES_PERSONS foreign key (PersonId) references [Demkiv].[PERSONS] (PersonId)
+
+alter table [Demkiv].[EMAILS]
+add constraint  fk_EMAILS_PERSONS foreign key (PersonId) references [Demkiv].[PERSONS] (PersonId)
+
+alter table [Demkiv].[EMPLOYEES]
+add constraint  fk_EMPLOYEES_PERSONS foreign key (PersonId) references [Demkiv].[PERSONS] (PersonId)
+alter table [Demkiv].[EMPLOYEES]
+add constraint  fk_EMPLOYEES_STATIONS foreign key (StationId) references [Demkiv].[STATIONS] (StationId)
+
+alter table [Demkiv].[CUSTOMERS]
+add constraint  fk_CUSTOMERS_PERSONS foreign key (PersonId) references [Demkiv].[PERSONS] (PersonId)
+alter table [Demkiv].[CUSTOMERS]
+add constraint  fk_CUSTOMERS_EMPLOYEES foreign key (EmployeeId) references [Demkiv].[EMPLOYEES] ([EmployeeId])
+
+alter table [Demkiv].[CARDS]
+add constraint  fk_CARDS_CUSTOMERS foreign key (CustomerId) references [Demkiv].[CUSTOMERS] (CustomerId)
 
 
+alter table [Demkiv].[ORDERS]
+add constraint  fk_ORDERS_CARDS foreign key (CardId) references [Demkiv].[CARDS] (CardNumber)
+alter table [Demkiv].[ORDERS]
+add constraint  fk_ORDERS_STATIONS foreign key (StationId) references [Demkiv].[STATIONS] (StationId)
+alter table [Demkiv].[ORDERS]
+add constraint  fk_ORDERS_PERSONS foreign key (PersonId) references [Demkiv].[PERSONS] (PersonId)
+
+alter table [Demkiv].[ORDERDETAILS]
+add constraint  fk_ORDERSDETAILS_ORDERS foreign key (OrderID) references [Demkiv].[ORDERS] (OrderID)
+alter table [Demkiv].[ORDERDETAILS]
+add constraint  fk_ORDERSDETAILS_PRODUCTS foreign key ([VendorID],[ProductId]) references [Demkiv].[PRODUCTS] ([VendorID],[ProductId])
 
 
+alter table [Demkiv].[STATIONS]
+add constraint  fk_STATIONS_EMPLOYEES foreign key (LeadId) references [Demkiv].[EMPLOYEES] ([EmployeeId]) 
+
+--Drop all the forign keys
+
+ ALTER TABLE [Demkiv].[PHONES] DROP CONSTRAINT [fk_PHONES_PERSONS]
+ ALTER TABLE [demkiv].[ORDERS] DROP CONSTRAINT [fk_ORDERS_CARDS]
+ ALTER TABLE [demkiv].[ORDERS] DROP CONSTRAINT [fk_ORDERS_PERSONS]
+ ALTER TABLE [demkiv].[ORDERS] DROP CONSTRAINT [fk_ORDERS_STATIONS]
+ ALTER TABLE [demkiv].[ORDERDETAILS] DROP CONSTRAINT [fk_ORDERSDETAILS_ORDERS]
+ ALTER TABLE [demkiv].[ORDERDETAILS] DROP CONSTRAINT [fk_ORDERSDETAILS_PRODUCTS]
+ ALTER TABLE [demkiv].[EMPLOYEES] DROP CONSTRAINT [fk_EMPLOYEES_PERSONS]
+ ALTER TABLE [demkiv].[EMPLOYEES] DROP CONSTRAINT [fk_EMPLOYEES_STATIONS]
+ ALTER TABLE [demkiv].[EMAILS] DROP CONSTRAINT [fk_EMAILS_PERSONS]
+ ALTER TABLE [demkiv].[CUSTOMERS] DROP CONSTRAINT [fk_CUSTOMERS_EMPLOYEES]
+ ALTER TABLE [demkiv].[CUSTOMERS] DROP CONSTRAINT [fk_CUSTOMERS_PERSONS]
+ ALTER TABLE [demkiv].[CARDS] DROP CONSTRAINT [fk_CARDS_CUSTOMERS]
 
 
-
-
-
-*/
