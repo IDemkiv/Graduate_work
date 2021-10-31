d99e0056b8562cd8376929d05447e7985cad57fc
IDemkiv <irina.nikolyaevna@gmail.com>
2021-10-31 22:11:08 +0200

    added trigger and procedure

--- Programming_objects.sql
+++ Programming_objects.sql
@@ -26,3 +26,76 @@ create function dbo.getEmp (@JobTitle nchar(30))
 
        end;
 select dbo.getEmp('Civil Engineer') 
+
+
+
+
+
+
+/** Creating trigger**/
+go
+create trigger PROD_QTY_CHECK_TR on [demkiv].[ORDERDETAILS]
+	after insert as
+		begin
+
+
+            declare @inappropriate_order_qty as int = ( 
+														select count(distinct t1.OrderID)
+														  from inserted                 as t1  
+														 inner join [demkiv].[PRODUCTS] as t2 on t2.VendorID = t1.VendorId
+																							 and t2.ProductId = t1.ProductId
+																							 and t2.ProductAmount < t1.OrderAmount);
+
+			if @inappropriate_order_qty > 0
+				rollback transaction;
+					
+		end;
+		
+
+
+/**Creating procedure**/
+go
+create procedure demkiv.CREATE_NEW_ORDER
+	@ORDER_ID bigint,
+	@ORDER_DETAIL_ID bigint,
+	@VENDOR_ID   bigint,
+	@PRODUCT_ID  bigint,
+	@QTY      int,
+	@SUM money
+	as begin
+	     
+		begin transaction
+		begin try 
+	    declare @COST as numeric(15, 2) = (select t1.ProductPrice
+		                                      from [demkiv].[PRODUCTS] as t1
+											  where t1.VendorID = @VENDOR_ID
+											    and t1.ProductId = @PRODUCT_ID);
+
+		declare @AMOUNT as numeric(15, 2) = (@QTY * @COST)
+
+	    insert into [demkiv].[ORDERDETAILS] ([OrderID], [OrderDetailId], [VendorId], [ProductId], [OrderAmount], [TotalSum])
+		values(@ORDER_ID, @ORDER_DETAIL_ID, @VENDOR_ID, @PRODUCT_ID, @QTY, @AMOUNT);
+
+
+		update [demkiv].[PRODUCTS]
+		   set ProductAmount = t1.ProductAmount - @QTY
+		  from [demkiv].[PRODUCTS] as t1
+		 where t1.VendorID = @VENDOR_ID
+		   and t1.ProductId = @PRODUCT_ID
+		   and isnull(@QTY, 0) > 0
+		end try 
+		begin catch
+		rollback transaction;
+		end catch
+
+		   commit transaction
+	   end ;
+
+
+
+execute demkiv.CREATE_NEW_ORDER 26, 4,7,9, 1900, 154;
+
+
+
+
+	
